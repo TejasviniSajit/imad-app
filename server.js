391444c93c01d8bd49c7a30b900069d4f105dccb
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-08-24 22:51:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,8 +33,20 @@ app.get('/counter', function (req, res){
    res.send(counter.toString());
 });
 
-app.get('/article-one', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('article/:articleName', function(req, res){
+  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+  });
+  
 });
 
 //app.get('/article-two', function(req, res){
