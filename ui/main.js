d1497c24a979f0b769439144e7e482293d077158
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-08-26 21:18:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = "300px";
+    var interval = setInterval(moveRight, 100);
+    //img.style.marginLeft = "300px";
 };
 
 //counter code
