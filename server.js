ea068b5b0a8daf87c31d809c1406c87ff7e778b6
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-08-28 13:06:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ var config = {
 };
 
 
-
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
@@ -75,7 +74,7 @@ app.get('/counter', function (req, res){
 });
 
 app.get('/articles/:articleName', function(req, res){
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
+    pool.query("SELECT * FROM article WHERE title = $1" [req.params.articleName], function (err, result){
         if (err) {
             res.status(500).send(err.toString());
         } else {
