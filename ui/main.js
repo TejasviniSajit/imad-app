adc03a82756f3814790d9c8d8fc93b1b32480e9d
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-08-28 22:44:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,26 +10,8 @@ img.onclick = function () {
     //img.style.marginLeft = "300px";
 };
 
-//counter code
-var button = document.getElementById('counter');
 
-button.onclick = function () {
-  var request = new XMLHttpRequest();
-  
-  request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-  };
-  
-  request.open('GET', 'http://tejasviniforensics.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
-
+// Submit username/passwprd to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
@@ -37,20 +19,21 @@ submit.onclick = function() {
   request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i<names.length; i++){
-                list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-          }
+                console.log('User logged in');
+                alert("Logged in successfully!");
+            } else if (request.status === 403) {
+                alert('username/password is incorrect');
+            } else if (request.status === 500) {
+                alert("Something went wrong on the server")
+            }
       }
   };
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://tejasviniforensics.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://tejasviniforensics.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
 };
