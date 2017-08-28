d088564782377d12ade619a2ecb1ec33cf888dc7
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-08-28 21:43:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
-var Pool = require('pg').Pool;
-var crypto = require('crypto');
+
 var config = {
     user: 'tejasviniforensics',
     database: 'tejasviniforensics',
@@ -65,6 +68,20 @@ function hash (input, salt) {
 app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created: ' + username);
+        }
+    });
 });
 
 var pool = new Pool(config);
