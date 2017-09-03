9236e5ae976cba44480257db6bfcff3a96cf8846
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-09-03 15:53:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,11 +20,11 @@ function loadLoginForm () {
         <input type="submit" id="register_btn" value="Register" />
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
-
-
-    // Submit username/passwprd to login
-    var submit = document.getElementById('submit_btn');
-    submit.onclick = function() {
+    
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
         var request = new XMLHttpRequest();
         
         // Capture the response and store it in a variable
@@ -43,18 +43,22 @@ function loadLoginForm () {
                   submit.value = 'Login';
               }
               loadLogin();
-            }
+          }  
+          // Not done yet
         };
+        
+        // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', 'http://tejasviniforensics.imad.hasura-app.io/login', true);
+        request.open('POST', '/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));
+        request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
+        
     };
-   
+    
     var register = document.getElementById('register_btn');
     register.onclick = function () {
         // Create a request object
@@ -86,7 +90,6 @@ function loadLoginForm () {
     
     };
 }
-
 
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
@@ -139,5 +142,9 @@ function loadArticles () {
     request.send(null);
 }
 
+
+// The first thing to do is to check if the user is logged in!
 loadLogin();
-loadArticles ();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
