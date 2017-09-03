7cfc47a0943641becae51df33abad2a8a19b48d8
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-09-03 14:55:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
   
-  request.onreadystatechange = function() {
+    request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
                 console.log('User logged in');
@@ -36,4 +36,30 @@ submit.onclick = function() {
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
     
+};
+
+// Register a new user
+var register = document.getElementById('Register_btn');
+register.onclick = function() {
+    var request = new XMLHttpRequest();
+  
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+                console.log('User logged in');
+                alert("Logged in successfully!");
+            } else if (request.status === 403) {
+                alert('username/password is incorrect');
+            } else if (request.status === 500) {
+                alert("Something went wrong on the server")
+            }
+      }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://tejasviniforensics.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
