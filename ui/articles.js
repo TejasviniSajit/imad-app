0c8b39cf5f11340207c2e72bb63cff246db8ac19
TejasviniSajit <30803835+TejasviniSajit@users.noreply.github.com>
2017-09-03 18:11:58 +0530

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -47,7 +47,7 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                loadCommentForm(/*this.responseText*/);
+                loadCommentForm(this.responseText);
             }
         }
     };
